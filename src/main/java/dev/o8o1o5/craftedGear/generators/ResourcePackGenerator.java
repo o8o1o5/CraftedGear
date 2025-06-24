8c2608a5763ce40bd8afcbd0d9bfd3bce89c4610
경열 <sikadeer105@gmail.com>
2025-06-24 15:35:25 +0900

    으악오늘 학교도 끝

--- src/main/java/dev/o8o1o5/craftedGear/generators/ResourcePackGenerator.java
+++ src/main/java/dev/o8o1o5/craftedGear/generators/ResourcePackGenerator.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class ResourcePackGenerator {
 
@@ -23,16 +24,52 @@ public class ResourcePackGenerator {
         File outputZipFile = new File(dataFolder, "crafted_gear_pack.zip");
 
         if (buildFolder.exists()) {
-
+            deleteFolder(buildFolder);
         }
         buildFolder.mkdirs();
+
+        createPackMcMeta(buildFolder);
+
+        // 텍스쳐 불러오기
+        plugin.getLogger().info("텍스쳐 파일 불러오기...");
+
+        zipFolder(buildFolder, outputZipFile);
+        plugin.getLogger().info("리소스팩 ZIP 파일 생성하기...");
+
+        String sha1 = calculateSha1(outputZipFile);
+        plugin.getLogger().info("리소스팩 SHA1 해시 생성하기...");
+
+        deleteFolder(buildFolder);
+
+        return outputZipFile;
     }
-    
-    private void deleteFoler(File folder) {
-        // 삭제 로직
+
+    private void deleteFolder(File folder) {
+        // 폴더 내용을 재귀적으로 삭제하는 로직 구현
     }
-    
+
     private void createPackMcMeta(File buildFolder) throws IOException {
-        // pack.mcmeta 생성 로직
+        // pack.mcmeta 파일 생성 로직
+    }
+
+    private void copyResourceFromJar(String resourceInJarPath, File buildFolder, String targetRelativePath) throws IOException {
+        // JAR 내 리소스를 buildFolder 내 targetRelativePath로 복사
+    }
+
+    private void createCustomModelJson(File buildFolder, String modelName, String texturePath) throws IOException {
+        // 커스텀 모델 JSON 파일 생성 로직
+    }
+
+    private void createVanillaModelOverrideJson(File buildFolder, String vanillaItemName, List<Object> overrides) throws IOException {
+        // 바닐라 아이템 모델 오버라이드 JSON 생성 로직
+    }
+
+    private void zipFolder(File sourceFolder, File outputZipFile) throws IOException {
+        // 폴더를 ZIP 파일로 압축하는 로직
+    }
+
+    private String calculateSha1(File file) throws IOException, NoSuchAlgorithmException {
+        // SHA1 해시 계산 로직
+        return ""; // 실제 해시 값 반환
     }
 }
